29c38fdf0bd88db1476cfb448bab2a05da24508b
Elias Bernstein <eliasbernstein@gmail.com>
2025-12-07 13:42:07 -0500

    Missing tailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ export default {
     content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
